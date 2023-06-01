09491741f6fede2c06a58b8106efdffd88393070
bechara0 <jjbechara@gmail.com>
2023-06-01 16:43:55 -0300

    andando

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import MiComponente from "./MiComponente";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<header className="App-header">
+				<img src={logo} className="App-logo" alt="logo" />
+				<p>Bienvenido al Master en React</p>
+				{/*cargar primer componente*/}
+				<MiComponente />
+			</header>
+		</div>
+	);
 }
 
 export default App;
---
+++ src/MiComponente.js
@@ -0,0 +1,10 @@
+// importar módulos react
+import React from "react";
+
+// función del componente
+const MiComponente = () => {
+	return <p>Este es mi primer componente</p>;
+};
+
+// exports
+export default MiComponente;
