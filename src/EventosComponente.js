74f9e634e8ccea1912c3773eb77308f191cf3b38
bechara0 <jjbechara@gmail.com>
2023-06-02 15:49:07 -0300

    final

--- src/EventosComponente.js
+++ src/EventosComponente.js
@@ -1,15 +1,54 @@
 import React from "react";
 
 export const EventosComponente = () => {
-	const darClick = (e) => {
-		alert("Tremendo Click!!!");
+	const darClick = (e, nombre) => {
+		alert("Tremendo Click!!! " + nombre);
 	};
+
+	function darDobleClick(e) {
+		alert("Has dado doble click");
+	}
+
+	const hasEntrado = (e, accion) => {
+		alert("Has " + accion + " a la caja con el mousse!");
+	};
+
+	const estasDentro = (e) => {
+		console.log("Estas dentro el input, mete el nombre");
+	};
+
+	const estasFuera = (e) => {
+		console.log("Estas fuera el input, mete el nombre");
+	};
+
 	return (
 		<div>
 			<h1>Eventos en React</h1>
 
-			{/* Evento click */}
-			<button onClick={darClick}>Dame Click</button>
+			<p>
+				{/* Evento click */}
+				<button onClick={(e) => darClick(e, "Juancho")}>Dame Click</button>
+			</p>
+			<p>
+				{/* Evento Doble click */}
+				<button onDoubleClick={darDobleClick}>Dame Doble Click</button>
+			</p>
+			<div
+				id="caja"
+				onMouseEnter={(e) => hasEntrado(e, "entrado")}
+				onMouseLeave={(e) => hasEntrado(e, "salido")}
+			>
+				{/* Evento mousse enter mousse leave */}
+				Pasa por encima!
+			</div>
+			<p>
+				<input
+					type="text"
+					placeholder="Introduce tu nombre"
+					onFocus={estasDentro}
+					onBlur={estasFuera}
+				></input>
+			</p>
 		</div>
 	);
 };
